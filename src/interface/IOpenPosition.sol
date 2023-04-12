129e234168ef113c7c954ce999635746934aaab8
Keating <keating.dev@protonmail.com>
2023-04-11 23:16:36 -0400

    Update to pass linting

--- src/interface/IOpenPosition.sol
+++ src/interface/IOpenPosition.sol
@@ -2,16 +2,8 @@
 pragma solidity >=0.8.0;
 
 interface IOpenPosition {
-    function openPosition(
-        uint256 amount,
-        uint256 oppositeAmountBound,
-        uint160 sqrtPriceLimitX96
-    )
-        external
-        payable
-        returns (
-            uint256 base,
-            uint256 quote,
-            uint256 fee
-        );
+  function openPosition(uint256 amount, uint256 oppositeAmountBound, uint160 sqrtPriceLimitX96)
+    external
+    payable
+    returns (uint256 base, uint256 quote, uint256 fee);
 }
--- test/Perpertual.t.sol
+++ test/Perpertual.t.sol
@@ -9,32 +9,29 @@ import {IOpenPosition} from "src/interface/IOpenPosition.sol";
 import {IDelegateApproval} from "test/interface/IDelegateApproval.sol";
 
 contract RouterForkTestBase is Test {
-    IClearingHouse clearingHouse =
-        IClearingHouse(0x82ac2CE43e33683c58BE4cDc40975E73aA50f459);
-    IVault vault = IVault(0xAD7b4C162707E0B2b5f6fdDbD3f8538A5fbA0d60);
-    IDelegateApproval delegateApproval =
-        IDelegateApproval(0xfd7bB5F6844a43c5469c972640Eddfa99597a547);
-    address public immutable vWETH = 0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB;
+  IClearingHouse clearingHouse = IClearingHouse(0x82ac2CE43e33683c58BE4cDc40975E73aA50f459);
+  IVault vault = IVault(0xAD7b4C162707E0B2b5f6fdDbD3f8538A5fbA0d60);
+  IDelegateApproval delegateApproval = IDelegateApproval(0xfd7bB5F6844a43c5469c972640Eddfa99597a547);
+  address public immutable VWETH = 0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB;
 
-    PerpetualRouterFactory factory;
-    uint256 optimismForkId;
+  PerpetualRouterFactory factory;
+  uint256 optimismForkId;
 
-    function setUp() public {
-        optimismForkId = vm.createSelectFork(vm.rpcUrl("optimism"), 87_407_144);
-        factory = new PerpetualRouterFactory(clearingHouse);
-        factory.deploy(vWETH);
-        deal(address(this), 100 ether);
-        vault.depositEther{value: 10 ether}();
-    }
+  function setUp() public {
+    optimismForkId = vm.createSelectFork(vm.rpcUrl("optimism"), 87_407_144);
+    factory = new PerpetualRouterFactory(clearingHouse);
+    factory.deploy(VWETH);
+    deal(address(this), 100 ether);
+    vault.depositEther{value: 10 ether}();
+  }
 }
 
 contract OpenPositionLongInputFork is RouterForkTestBase {
-    function testOpenPosition() public {
-        delegateApproval.approve(address(factory.computeAddress(vWETH)), 1); //
-        (uint256 base, uint256 quote, ) = IOpenPosition(
-            factory.computeAddress(vWETH)
-        ).openPosition(1 ether, 0, 0);
-        assertEq(quote, 1 ether);
-        assertEq(base, 538_599_759_293_451);
-    }
+  function testOpenPosition() public {
+    delegateApproval.approve(address(factory.computeAddress(VWETH)), 1); //
+    (uint256 base, uint256 quote,) =
+      IOpenPosition(factory.computeAddress(VWETH)).openPosition(1 ether, 0, 0);
+    assertEq(quote, 1 ether);
+    assertEq(base, 538_599_759_293_451);
+  }
 }
