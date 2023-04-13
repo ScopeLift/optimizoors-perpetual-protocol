22d9ac2b979f9c73af3c85526ca3dc4027b7e1de
Keating <keating.dev@protonmail.com>
2023-04-12 21:31:25 -0400

    Fix formatting

--- test/Perpertual.t.sol
+++ test/Perpertual.t.sol
@@ -9,49 +9,44 @@ import {IOpenPosition} from "src/interface/IOpenPosition.sol";
 import {IDelegateApproval} from "test/interface/IDelegateApproval.sol";
 
 contract RouterFactoryTest is Test {
-    function test_deployRouter() public {
-        address VETH = 0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB;
+  function test_deployRouter() public {
+    address VETH = 0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB;
 
-        // Random address
-        IClearingHouse clearingHouse = IClearingHouse(
-            0x82ac2CE43e33683c58BE4cDc40975E73aA50f459
-        );
-        PerpetualRouterFactory factory = new PerpetualRouterFactory(
+    // Random address
+    IClearingHouse clearingHouse = IClearingHouse(0x82ac2CE43e33683c58BE4cDc40975E73aA50f459);
+    PerpetualRouterFactory factory = new PerpetualRouterFactory(
             clearingHouse
         );
-        address VETHLongInputRouter = factory.deploy(VETH);
-        assertEq(VETHLongInputRouter, factory.computeAddress(VETH));
-    }
+    address VETHLongInputRouter = factory.deploy(VETH);
+    assertEq(VETHLongInputRouter, factory.computeAddress(VETH));
+  }
 }
 
 contract RouterForkTestBase is Test {
-    // Optimism addresses
+  // Optimism addresses
 
-    IClearingHouse clearingHouse =
-        IClearingHouse(0x82ac2CE43e33683c58BE4cDc40975E73aA50f459);
-    IVault vault = IVault(0xAD7b4C162707E0B2b5f6fdDbD3f8538A5fbA0d60);
-    IDelegateApproval delegateApproval =
-        IDelegateApproval(0xfd7bB5F6844a43c5469c972640Eddfa99597a547);
-    address public immutable VETH = 0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB;
+  IClearingHouse clearingHouse = IClearingHouse(0x82ac2CE43e33683c58BE4cDc40975E73aA50f459);
+  IVault vault = IVault(0xAD7b4C162707E0B2b5f6fdDbD3f8538A5fbA0d60);
+  IDelegateApproval delegateApproval = IDelegateApproval(0xfd7bB5F6844a43c5469c972640Eddfa99597a547);
+  address public immutable VETH = 0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB;
 
-    PerpetualRouterFactory factory;
+  PerpetualRouterFactory factory;
 
-    function setUp() public {
-        vm.createSelectFork(vm.rpcUrl("optimism"), 87_407_144);
-        factory = new PerpetualRouterFactory(clearingHouse);
-        factory.deploy(VETH);
-        deal(address(this), 100 ether);
-        vault.depositEther{value: 10 ether}();
-    }
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl("optimism"), 87_407_144);
+    factory = new PerpetualRouterFactory(clearingHouse);
+    factory.deploy(VETH);
+    deal(address(this), 100 ether);
+    vault.depositEther{value: 10 ether}();
+  }
 }
 
 contract OpenPositionLongInputFork is RouterForkTestBase {
-    function test_OpenPosition() public {
-        delegateApproval.approve(address(factory.computeAddress(VETH)), 1); //
-        (uint256 base, uint256 quote, ) = IOpenPosition(
-            factory.computeAddress(VETH)
-        ).openPosition(1 ether, 0, 0);
-        assertEq(quote, 1 ether);
-        assertEq(base, 538_599_759_293_451);
-    }
+  function test_OpenPosition() public {
+    delegateApproval.approve(address(factory.computeAddress(VETH)), 1); //
+    (uint256 base, uint256 quote,) =
+      IOpenPosition(factory.computeAddress(VETH)).openPosition(1 ether, 0, 0);
+    assertEq(quote, 1 ether);
+    assertEq(base, 538_599_759_293_451);
+  }
 }
