215b9df2988a70152b873bfbf8493b2909c8b5cb
Keating <keating.dev@protonmail.com>
2023-04-14 19:02:15 -0400

    Working closePosition

--- src/Perpetual.sol
+++ src/Perpetual.sol
@@ -3,7 +3,10 @@ pragma solidity >=0.8.0;
 
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {IClearingHouse} from "src/interface/IClearingHouse.sol";
+import {IAccountBalance} from "src/interface/IAccountBalance.sol";
 import {Create2} from "src/lib/Create2.sol";
+
+import {SignedMath} from "openzeppelin-contracts/contracts/utils/math/SignedMath.sol";
 
 abstract contract PerpetualBaseRouter {
   /// @notice The contract used to manage positions in perpetual
@@ -16,28 +19,131 @@ abstract contract PerpetualBaseRouter {
   bool public immutable IS_EXACT_INPUT;
   /// @notice The token used for the router's positions
   address public immutable TOKEN;
+  IAccountBalance public immutable ACCOUNT_BALANCE;
 
   /// @dev A null value for a referral code
   bytes32 internal constant REFERRAL_CODE =
     0x0000000000000000000000000000000000000000000000000000000000000000;
 
-  constructor(IClearingHouse clearingHouse, address asset, bool isBaseToQuote, bool isExactInput) {
+  constructor(
+    IClearingHouse clearingHouse,
+    address asset,
+    bool isBaseToQuote,
+    bool isExactInput,
+    IAccountBalance accountBalance
+  ) {
     PERPETUAL_CLEARING_HOUSE = clearingHouse;
     IS_BASE_TO_QUOTE = isBaseToQuote;
     IS_EXACT_INPUT = isExactInput;
     TOKEN = asset;
+    ACCOUNT_BALANCE = accountBalance;
   }
 }
 
 // TODO: Add a router for each combination of long/short and exact input/output.
-//
-// Add fallback
-contract PerpetualLongInput is PerpetualBaseRouter {
-  constructor(IClearingHouse perpetual, address asset)
-    PerpetualBaseRouter(perpetual, asset, false, true)
+contract PerpetualPositionRouter is PerpetualBaseRouter {
+  using SignedMath for int256;
+
+  error FunctionDoesNotExist();
+  error NoExistingPosition();
+
+  constructor(IClearingHouse perpetual, IAccountBalance accountBalance, address asset)
+    PerpetualBaseRouter(perpetual, asset, false, true, accountBalance)
   {}
 
   receive() external payable {}
+
+  function _openLongInput(uint256 amount, uint256 oppositeAmountBound, uint160 sqrtPriceLimitX96)
+    private
+  {
+    PERPETUAL_CLEARING_HOUSE.openPositionFor(
+      msg.sender,
+      IClearingHouse.OpenPositionParams({
+        baseToken: TOKEN,
+        isBaseToQuote: false,
+        isExactInput: true,
+        amount: amount,
+        oppositeAmountBound: oppositeAmountBound,
+        deadline: type(uint256).max,
+        sqrtPriceLimitX96: sqrtPriceLimitX96,
+        referralCode: REFERRAL_CODE
+      })
+    );
+  }
+
+  function _openLongOutput(uint256 amount, uint256 oppositeAmountBound, uint160 sqrtPriceLimitX96)
+    private
+  {
+    PERPETUAL_CLEARING_HOUSE.openPositionFor(
+      msg.sender,
+      IClearingHouse.OpenPositionParams({
+        baseToken: TOKEN,
+        isBaseToQuote: false,
+        isExactInput: false,
+        amount: amount,
+        oppositeAmountBound: oppositeAmountBound,
+        deadline: type(uint256).max,
+        sqrtPriceLimitX96: sqrtPriceLimitX96,
+        referralCode: REFERRAL_CODE
+      })
+    );
+  }
+
+  function _openShortInput(uint256 amount, uint256 oppositeAmountBound, uint160 sqrtPriceLimitX96)
+    private
+  {
+    PERPETUAL_CLEARING_HOUSE.openPositionFor(
+      msg.sender,
+      IClearingHouse.OpenPositionParams({
+        baseToken: TOKEN,
+        isBaseToQuote: true,
+        isExactInput: true,
+        amount: amount,
+        oppositeAmountBound: oppositeAmountBound,
+        deadline: type(uint256).max,
+        sqrtPriceLimitX96: sqrtPriceLimitX96,
+        referralCode: REFERRAL_CODE
+      })
+    );
+  }
+
+  function _openShortOutput(uint256 amount, uint256 oppositeAmountBound, uint160 sqrtPriceLimitX96)
+    private
+  {
+    PERPETUAL_CLEARING_HOUSE.openPositionFor(
+      msg.sender,
+      IClearingHouse.OpenPositionParams({
+        baseToken: TOKEN,
+        isBaseToQuote: true,
+        isExactInput: false,
+        amount: amount,
+        oppositeAmountBound: oppositeAmountBound,
+        deadline: type(uint256).max, // TODO: verify this is market order behavior, and do we need a
+        // separate limit router
+        sqrtPriceLimitX96: sqrtPriceLimitX96,
+        referralCode: REFERRAL_CODE
+      })
+    );
+  }
+
+  function _closePosition(uint256 oppositeAmountBound, uint160 sqrtPriceLimitX96) private {
+    int256 takerPositionSize = ACCOUNT_BALANCE.getTakerPositionSize(msg.sender, TOKEN);
+    if (takerPositionSize == 0) revert NoExistingPosition();
+    bool shortPosition = takerPositionSize > 0 ? true : false;
+    PERPETUAL_CLEARING_HOUSE.openPositionFor(
+      msg.sender,
+      IClearingHouse.OpenPositionParams({
+        baseToken: TOKEN,
+        isBaseToQuote: shortPosition,
+        isExactInput: shortPosition,
+        amount: takerPositionSize.abs(),
+        oppositeAmountBound: oppositeAmountBound,
+        deadline: type(uint256).max,
+        sqrtPriceLimitX96: sqrtPriceLimitX96,
+        referralCode: REFERRAL_CODE
+      })
+    );
+  }
 
   // TODO: Greater optimization is possible but it will
   //   add more complexity. In order to avoid going
@@ -50,32 +156,30 @@ contract PerpetualLongInput is PerpetualBaseRouter {
   //
   // 1. What us a reasonable amount of precision to reduce the function?
   // 2. What are reasonable time periods for deadlines
+  //
+  // closePosition is not using the amount value and maybe we could optimize by splitting it into a
+  // separate contract
   fallback() external payable {
-    (uint256 amount, uint256 oppositeAmountBound, uint160 sqrtPriceLimitX96) =
-      abi.decode(msg.data, (uint256, uint256, uint160));
-    PERPETUAL_CLEARING_HOUSE.openPositionFor(
-      msg.sender,
-      IClearingHouse.OpenPositionParams({
-        baseToken: TOKEN,
-        isBaseToQuote: IS_BASE_TO_QUOTE,
-        isExactInput: IS_EXACT_INPUT,
-        amount: amount,
-        oppositeAmountBound: oppositeAmountBound,
-        deadline: block.timestamp,
-        sqrtPriceLimitX96: sqrtPriceLimitX96,
-        referralCode: REFERRAL_CODE
-      })
-    );
+    (uint8 funcName, uint256 amount, uint256 oppositeAmountBound, uint160 sqrtPriceLimitX96) =
+      abi.decode(msg.data, (uint8, uint256, uint256, uint160));
+    if (funcName == 1) _openShortOutput(amount, oppositeAmountBound, sqrtPriceLimitX96);
+    else if (funcName == 2) _openShortInput(amount, oppositeAmountBound, sqrtPriceLimitX96);
+    else if (funcName == 3) _openLongOutput(amount, oppositeAmountBound, sqrtPriceLimitX96);
+    else if (funcName == 4) _openLongInput(amount, oppositeAmountBound, sqrtPriceLimitX96);
+    else if (funcName == 5) _closePosition(oppositeAmountBound, sqrtPriceLimitX96);
+    else revert FunctionDoesNotExist();
   }
 }
 
 contract PerpetualRouterFactory {
   IClearingHouse public immutable PERPETUAL_CLEARING_HOUSE;
+  IAccountBalance public immutable PERPETUAL_ACCOUNT_BALANCE;
 
   event RouterDeployed(string indexed routerType, address indexed asset);
 
-  constructor(IClearingHouse clearingHouse) {
+  constructor(IClearingHouse clearingHouse, IAccountBalance accountBalance) {
     PERPETUAL_CLEARING_HOUSE = clearingHouse;
+    PERPETUAL_ACCOUNT_BALANCE = accountBalance;
   }
 
   // TODO: Modify to support multiple router types
@@ -83,8 +187,13 @@ contract PerpetualRouterFactory {
   //     router.
   function deploy(address asset) external returns (address) {
     bytes32 salt = _salt(asset);
-    address openPositionLongInput =
-      address(new PerpetualLongInput{salt: salt}(PERPETUAL_CLEARING_HOUSE, asset));
+    address openPositionLongInput = address(
+      new PerpetualPositionRouter{salt: salt}(
+                PERPETUAL_CLEARING_HOUSE,
+                PERPETUAL_ACCOUNT_BALANCE,
+                asset
+            )
+    );
     emit RouterDeployed("longInput", asset);
     return openPositionLongInput;
   }
@@ -93,8 +202,8 @@ contract PerpetualRouterFactory {
     return Create2.computeCreate2Address(
       _salt(asset),
       address(this),
-      type(PerpetualLongInput).creationCode,
-      abi.encode(PERPETUAL_CLEARING_HOUSE, asset)
+      type(PerpetualPositionRouter).creationCode,
+      abi.encode(PERPETUAL_CLEARING_HOUSE, PERPETUAL_ACCOUNT_BALANCE, asset)
     );
   }
 
--- test/Perpertual.t.sol
+++ test/Perpertual.t.sol
@@ -4,26 +4,12 @@ pragma solidity >=0.8.0;
 import "forge-std/Test.sol";
 import {IClearingHouse} from "src/interface/IClearingHouse.sol";
 import {IVault} from "src/interface/IVault.sol";
-import {PerpetualRouterFactory, PerpetualLongInput} from "src/Perpetual.sol";
+import {PerpetualRouterFactory} from "src/Perpetual.sol";
 import {IAccountBalance} from "src/interface/IAccountBalance.sol";
 import {AccountMarket} from "src/lib/AccountMarket.sol";
 import {IDelegateApproval} from "test/interface/IDelegateApproval.sol";
 
-contract RouterFactoryTest is Test {
-  function test_deployRouter() public {
-    // Opitimism addresses
-
-    address VETH = 0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB;
-    IClearingHouse clearingHouse = IClearingHouse(0x82ac2CE43e33683c58BE4cDc40975E73aA50f459);
-    PerpetualRouterFactory factory = new PerpetualRouterFactory(
-            clearingHouse
-        );
-    address VETHLongInputRouter = factory.deploy(VETH);
-    assertEq(VETHLongInputRouter, factory.computeAddress(VETH));
-  }
-}
-
-contract RouterForkTestBase is Test {
+contract PerpetualContracts {
   // Optimism addresses
 
   IClearingHouse clearingHouse = IClearingHouse(0x82ac2CE43e33683c58BE4cDc40975E73aA50f459);
@@ -31,25 +17,114 @@ contract RouterForkTestBase is Test {
   IAccountBalance accountBalance = IAccountBalance(0xA7f3FC32043757039d5e13d790EE43edBcBa8b7c);
   IDelegateApproval delegateApproval = IDelegateApproval(0xfd7bB5F6844a43c5469c972640Eddfa99597a547);
   address public immutable VETH = 0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB;
+}
 
+contract RouterFactoryTest is Test, PerpetualContracts {
+  function test_deployRouter() public {
+    PerpetualRouterFactory factory = new PerpetualRouterFactory(
+            clearingHouse,
+            accountBalance
+        );
+
+    address VETHLongInputRouter = factory.deploy(VETH);
+    assertEq(VETHLongInputRouter, factory.computeAddress(VETH));
+  }
+}
+
+contract RouterForkTestBase is Test, PerpetualContracts {
   PerpetualRouterFactory factory;
 
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl("optimism"), 87_407_144);
-    factory = new PerpetualRouterFactory(clearingHouse);
+    factory = new PerpetualRouterFactory(clearingHouse, accountBalance);
     factory.deploy(VETH);
     deal(address(this), 100 ether);
     vault.depositEther{value: 10 ether}();
   }
+
+  function _closePositionHelper(
+    uint8 openFunc,
+    uint256 amount,
+    uint256 oppositeAmountBound,
+    uint256 sqrtPriceLimitX96
+  ) internal {
+    delegateApproval.approve(address(factory.computeAddress(VETH)), 1); //
+    (bool ok,) = payable(factory.computeAddress(VETH)).call(
+      abi.encode(openFunc, amount, oppositeAmountBound, sqrtPriceLimitX96)
+    );
+    (bool okTwo,) = payable(factory.computeAddress(VETH)).call(
+      abi.encode(5, 0, oppositeAmountBound, sqrtPriceLimitX96)
+    );
+    assertTrue(ok);
+    assertTrue(okTwo);
+  }
 }
 
 contract OpenPositionLongInputFork is RouterForkTestBase {
-  function test_Fallback() public {
+  function test_FallbackLongInput() public {
     delegateApproval.approve(address(factory.computeAddress(VETH)), 1); //
-    (bool ok,) = payable(factory.computeAddress(VETH)).call(abi.encode(1 ether, 0, 0));
+    (bool ok,) = payable(factory.computeAddress(VETH)).call(abi.encode(4, 1 ether, 0, 0));
     AccountMarket.Info memory info = accountBalance.getAccountInfo(address(this), VETH);
     assertTrue(ok);
     assertEq(info.takerOpenNotional, -1 ether);
     assertEq(info.takerPositionSize, 538_599_759_293_451);
+  }
+
+  function test_FallbackLongOutput() public {
+    delegateApproval.approve(address(factory.computeAddress(VETH)), 1); //
+    (bool ok,) = payable(factory.computeAddress(VETH)).call(abi.encode(3, 1 ether, 0, 0));
+    AccountMarket.Info memory info = accountBalance.getAccountInfo(address(this), VETH);
+
+    assertTrue(ok);
+    assertEq(info.takerOpenNotional, -1_856_697_038_719_929_142_024);
+    assertEq(info.takerPositionSize, 1 ether);
+  }
+
+  function test_FallbackShortInput() public {
+    delegateApproval.approve(address(factory.computeAddress(VETH)), 1); //
+    (bool ok,) = payable(factory.computeAddress(VETH)).call(abi.encode(2, 1 ether, 0, 0));
+    AccountMarket.Info memory info = accountBalance.getAccountInfo(address(this), VETH);
+    assertTrue(ok);
+    assertEq(info.takerOpenNotional, 1_852_924_032_181_202_909_050);
+    assertEq(info.takerPositionSize, -1 ether);
+  }
+
+  function test_FallbackShortOutput() public {
+    delegateApproval.approve(address(factory.computeAddress(VETH)), 1); //
+    (bool ok,) = payable(factory.computeAddress(VETH)).call(abi.encode(1, 1 ether, 0, 0));
+    AccountMarket.Info memory info = accountBalance.getAccountInfo(address(this), VETH);
+    assertTrue(ok);
+    assertEq(info.takerOpenNotional, 1 ether);
+    assertEq(info.takerPositionSize, -539_678_586_420_661);
+  }
+
+  function test_FallbackClosePositionLong() public {
+    _closePositionHelper(4, 1 ether, 0, 0);
+    AccountMarket.Info memory info = accountBalance.getAccountInfo(address(this), VETH);
+    assertEq(info.takerOpenNotional, 0);
+    assertEq(info.takerPositionSize, 0);
+  }
+
+  function test_FallbackClosePositionShortInput() public {
+    _closePositionHelper(2, 5 ether, 0, 0);
+
+    AccountMarket.Info memory info = accountBalance.getAccountInfo(address(this), VETH);
+    assertEq(info.takerOpenNotional, 0);
+    assertEq(info.takerPositionSize, 0);
+  }
+
+  function test_FallbackClosePositionShortOutput() public {
+    _closePositionHelper(1, 1 ether, 0, 0);
+    AccountMarket.Info memory info = accountBalance.getAccountInfo(address(this), VETH);
+    assertEq(info.takerOpenNotional, 0);
+    assertEq(info.takerPositionSize, 0);
+  }
+
+  function test_FallbackClosePositionLongOutput() public {
+    _closePositionHelper(3, 1 ether, 0, 0);
+
+    AccountMarket.Info memory info = accountBalance.getAccountInfo(address(this), VETH);
+    assertEq(info.takerOpenNotional, 0);
+    assertEq(info.takerPositionSize, 0);
   }
 }
