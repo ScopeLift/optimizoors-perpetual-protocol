0788025ed1f7da018b633ee2752327100edc12e9
Keating <keating.dev@protonmail.com>
2023-05-17 11:05:50 -0400

    Change pragma to ^0.8.0

--- src/PerpetualPositionRouter.sol
+++ src/PerpetualPositionRouter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
 import {SignedMath} from "openzeppelin-contracts/contracts/utils/math/SignedMath.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
