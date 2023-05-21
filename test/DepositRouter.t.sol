a198214c35456dfceed50a386a6285dff8c606ca
Keating <keating.dev@protonmail.com>
2023-05-20 22:26:34 -0400

    Clean up DepositRouter tests

--- test/DepositRouter.t.sol
+++ test/DepositRouter.t.sol
@@ -5,10 +5,11 @@ import {Test} from "forge-std/Test.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
+import {DepositRouter} from "src/DepositRouter.sol";
 import {PerpetualContracts} from "test/PerpetualContracts.sol";
 import {PerpetualRouterFactory} from "src/PerpetualRouterFactory.sol";
 
-contract DepositRouterForkTestBase is Test, PerpetualContracts {
+contract DepositRouterTest is Test, PerpetualContracts {
   PerpetualRouterFactory factory;
   address routerAddress;
 
@@ -16,29 +17,57 @@ contract DepositRouterForkTestBase is Test, PerpetualContracts {
     vm.createSelectFork(vm.rpcUrl("optimism"), 87_407_144);
     factory = new PerpetualRouterFactory(clearingHouse, accountBalance, vault);
     factory.deploy(PerpetualRouterFactory.RouterTypes.DepositRouterType, USDC);
-    deal(USDC, address(this), 100_000_000);
-    deal(address(this), 100 ether);
     routerAddress =
       address(factory.computeAddress(PerpetualRouterFactory.RouterTypes.DepositRouterType, USDC));
   }
 }
 
-// Add deposit interface
-contract DepositForkTest is DepositRouterForkTestBase {
-  function test_Deposit() public {
-    uint256 amount = 1_000_000;
+contract Constructor is DepositRouterTest {
+  function test_CorrectlySetsAllConstructorArgs() public {
+    DepositRouter router = new DepositRouter(
+  	  VETH,
+	  vault
+  );
+    assertEq(
+      address(router.PERPETUAL_VAULT()),
+      address(vault),
+      "VAULT not set correctly"
+    );
+    assertEq(router.TOKEN(), VETH, "TOKEN not set correctly");
+  }
+}
 
+contract Fallback is DepositRouterTest {
+  function testForkFuzz_DepositUsdc(uint256 amount) public {
+    uint256 settlementTokenBalanceCap = clearingHouseConfig.getSettlementTokenBalanceCap();
+	uint256 vaultBalance = ERC20(USDC).balanceOf(address(vault));
+
+    vm.assume(amount < settlementTokenBalanceCap - vaultBalance);
+    vm.assume(amount > 0);
+
+    deal(USDC, address(this), amount);
     ERC20(USDC).approve(routerAddress, amount);
+
     (bool ok,) = payable(routerAddress).call(abi.encode(amount));
+
     int256 balance = vault.getBalanceByToken(address(this), USDC);
     assertTrue(ok);
     assertEq(balance, int256(amount));
   }
+}
 
-  function test_DepositEth() public {
-    (bool ok,) = payable(routerAddress).call{value: 1 ether}("");
+contract Receive is DepositRouterTest {
+  function testForkFuzz_DepositEther(uint256 amount) public {
+    uint256 depositCap = collateralManager.getCollateralConfig(WETH).depositCap;
+	uint256 vaultBalance = ERC20(WETH).balanceOf(address(vault));
+
+    vm.assume(amount < depositCap - vaultBalance);
+    vm.assume(amount > 0);
+ 
+    deal(address(this), amount);
+    (bool ok,) = payable(routerAddress).call{value: amount}("");
     int256 balance = vault.getBalanceByToken(address(this), WETH);
     assertTrue(ok);
-    assertEq(balance, 1 ether);
+    assertEq(balance, int256(amount));
   }
 }
