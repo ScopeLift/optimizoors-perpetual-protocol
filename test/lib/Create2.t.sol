476e18ff77b3ccd0afb4a053b708c70e57ff0875
Matt Solomon <matt@mattsolomon.dev>
2023-05-26 12:59:41 -0700

    chore: rename var

--- test/lib/Create2.t.sol
+++ test/lib/Create2.t.sol
@@ -8,9 +8,9 @@ import {Create2} from "src/lib/Create2.sol";
 contract ComputeCreate2Address is Test {
   function test_GenerateCreate2Address() external {
     bytes32 salt = bytes32(uint256(31_415));
-    bytes memory initcodeHash = bytes.concat(keccak256(abi.encode(0x6080)));
+    bytes memory initcode = bytes.concat(keccak256(abi.encode(0x6080)));
     address deployer = 0x6C9FC64A53c1b71FB3f9Af64d1ae3A4931A5f4E9;
-    address create2Address = Create2.computeCreate2Address(salt, deployer, initcodeHash, "");
+    address create2Address = Create2.computeCreate2Address(salt, deployer, initcode, "");
     assertEq(create2Address, 0xD59D787ae6266c000214564aaE112b53220Ceb2B);
   }
 }
